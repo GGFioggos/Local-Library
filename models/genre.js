9616f24a7fb20ef69954737be4f1f82e063c9113
moschidisg <mosxidismosxidis@gmail.com>
2022-11-01 22:16:27 +0200

    initial commit

---
+++ models/genre.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 100,
+        enum: [
+            'fiction',
+            'non fiction',
+            'romance',
+            'military history',
+            'Fantasy',
+            'Science Fiction',
+            'French Poetry',
+        ],
+    },
+});
+
+GenreSchema.virtual('url').get(function () {
+    return `catalog/genre/${this._id}`;
+});
+
+module.exports = mongoose.model('Genre', GenreSchema);
